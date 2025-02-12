04dc15bc336b9153d452fef070b511f548edfc77
altafalinadaf <altafalinadaf@gmail.com>
2025-02-12 14:55:21 +0530

    adding 2 program

---
+++ Program/program.py
@@ -0,0 +1,4 @@
+a ="computer"
+
+print(a[0:4])
+print(a[-1:-5:-1])
---
+++ Program/program2.py
@@ -0,0 +1,8 @@
+str="khadar basha"
+
+print(str.split('a'))
+
+print(str.replace('a','e'))
+
+# by using id() we can see the variable address 
+print(id(str))
