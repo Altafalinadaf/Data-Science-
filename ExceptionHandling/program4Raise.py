9dc2675e8b29e73ed54ee9bd4eb677a069faac55
altafalinadaf <altafalinadaf@gmail.com>
2025-02-15 14:31:58 +0530

    adding the raise Exception porgram

---
+++ ExceptionHandling/program4Raise.py
@@ -0,0 +1,23 @@
+while True:
+    try:
+        a= int(input("enter num 1 : "))
+        b=int(input("enter the num 2"))
+
+        if a<=0 or b<0 :
+            raise Exception ("negative number not allowed! please try again ")
+            c=a/b
+            print("div c is ",c)
+            break
+
+
+    except ValueError:
+        print("please inter the integer value ");
+    
+    except ZeroDivisionError:
+        print("please enter the non-zero denominator")
+    
+    except Exception as e:
+        print(e)
+
+ 
+
